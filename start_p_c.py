4a4d4f4eb8dddcf0a6330c18e52b3bcaf7d6cc8c
QinYuan <932087612@qq.com>
2018-05-16 17:08:48 +0800

    first commit

---
+++ start_p_c.py
@@ -0,0 +1,42 @@
+import subprocess
+
+
+#以下为官方运行参数
+start_consumer = "java -jar \
+                       -Xms2G \
+                       -Xmx2G \
+                       -Dlogs.dir=./consumer_logs \
+                       mesh-consumer.jar"
+					   
+start_small_provider = "java -jar \
+                             -Xms1G \
+                             -Xmx1G \
+                             -Ddubbo.protocol.port=20889 \
+                             -Ddubbo.application.qos.enable=false \
+                             -Dlogs.dir=./small_logs \
+                             mesh-provider.jar"
+
+start_medium_provider = "java -jar \
+                              -Xms2G \
+                              -Xmx2G \
+                              -Ddubbo.protocol.port=20890 \
+                              -Ddubbo.application.qos.enable=false \
+                              -Dlogs.dir=./medium_logs \
+                              mesh-provider.jar"
+							  
+start_large_provider = "java -jar \
+                             -Xms3G \
+                             -Xmx3G \
+                             -Ddubbo.protocol.port=20891 \
+                             -Ddubbo.application.qos.enable=false \
+                             -Dlogs.dir=./large_logs \
+                             mesh-provider.jar "
+
+
+async_run(start_consumer, "consumer")
+async_run(start_small_provider, "small_provider")
+async_run(start_medium_provider, "medium_provider")
+async_run(start_large_provider, "large_provider")
+
+#将运行该脚本的命令行窗口关闭即可关闭 脚本启动的那几个jvm
+
